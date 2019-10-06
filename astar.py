6a652f092c04a2cbffc57a97b5858bb583842807
Jack <jack@nbp-96-169.nbp.ruw.rutgers.edu>
2019-10-06 12:59:35 -0400

    finished forward

--- astar.py
+++ astar.py
@@ -1,21 +1,28 @@
 import os
 import sys
+import math
+import heapq
+from heapq import heappush
 from os import listdir
 
 class Node:
     def __init__(self, x, y, blocked):
         self.x = x
         self.y = y
-        self.g = 0
-        self.h = 0
-        self.f = 0
+        self.g = float("inf")
+        self.h = float("inf")
+        self.f = float("inf")
         self.parent = None
         self.blocked = blocked
         self.visited = False 
+
+    def __lt__(self, other): 
+        return self.f < other.f
+    def __gt__(self, other):
+        return self.f > other.f 
+    def __eq__(self, other):
+        return self.f == other.f
         
-
-
-    
 
 def promptUser():
     #Prompt user for maze number 
@@ -28,7 +35,7 @@ def promptUser():
 
     #Pompt user for algorithm
     while True: 
-        algo = raw_input('''Enter "f" for RFA*, "b" for RBA*, or "a" for AA*.\n''')
+        algo = raw_input('''Enter F for FORWARD, B for BACKWARD, or A for ADAPTIVE.\n''')
         if(algo != 'f' and algo != 'b' and algo != 'a'):
             print("Improper input. Try again.\n")
         else: 
@@ -40,10 +47,7 @@ def promptUser():
 
 #Take in maze number and return 2D array
 def loadMaze(mazeNum):
-    
     array = []
-    
-
     for f in listdir("mazes"):
         if f == "maze" + str(mazeNum) + ".txt":
             file = open(os.getcwd() + "/mazes/" + f)
@@ -65,20 +69,36 @@ def loadMaze(mazeNum):
 
 
 def execute(array, algo):
-    print('\n')
-    #Preconceived start and goal nodes 
-    startx = 4
-    starty = 2
-    goalx = 4
-    goaly = 4
-
-    print(array[0][0].x)
-    print(array[0][0].y)
-    print(array[0][0].blocked)
-
-
     if algo == 'f':
         print("Executing Repeated Forward A*")
+
+        openlist = []
+        closelist = []
+        goal = array[len(array)-1][len(array[0])-1]
+
+        #Calculate and set h-value for starting point
+        array[4][2].g = 0
+        array[4][2].h = manDis(array[4][2].x,array[4][2].y,goal.x,goal.y)
+        array[4][2].f = array[4][2].g + array[4][2].h
+        curr = array[4][2]
+        heapq.heappush(openlist,curr)
+        while(curr != goal):
+            neighbors = getNeighbors(curr,array)
+            for node in neighbors:
+                if (node not in openlist and node not in closelist and node.blocked == False):
+                    heapq.heappush(openlist,node)
+                    heapq.heapify(openlist)
+                    node.g = curr.g + 1
+                    node.h = manDis(curr.x,curr.y,goal.x,goal.y)
+                    f = node.g + node.h
+                    if (f < node.f):
+                        node.f = f
+                        node.parent = curr
+            closelist.append(curr)
+            curr = heapq.heappop(openlist)
+        print("Finished Forward")
+        printPath(goal,array)
+
     elif algo == 'b':
         print("Executing Repeated Backward A*")
     else: 
@@ -99,9 +119,65 @@ def execute(array, algo):
     print("----------")
     
 
+def printPath(goal, array):
+    while(goal.parent != None):
+        print('(' + str(goal.x) + ',' + str(goal.y) + ')')
+        goal = goal.parent
+    print('(' + str(goal.x) + ',' + str(goal.y) + ')')
+
+
 #Compute Manhattan Distance between 2 nodes
 def manDis(x1,y1,x2,y2):
     return(abs(x2-x1)+abs(y2-y1)) 
+
+
+
+#Get the neighbors of a node and return them in an array 
+def getNeighbors(curr, array):
+    neighbors = []
+    if(curr.x == 0 and curr.y == 0):                             #Top left corner 
+        neighbors.append(array[curr.x][curr.y+1])
+        neighbors.append(array[curr.x+1][curr.y])
+        return neighbors
+    elif (curr.x == 0 and curr.y == len(array)-1):               #Top right corner
+        neighbors.append(array[curr.x+1][curr.y])
+        neighbors.append(array[curr.x][curr.y-1])
+        return neighbors
+    elif (curr.x == len(array[0])-1 and curr.y == 0):            #Bottom left corner
+        neighbors.append(array[curr.x-1][curr.y])
+        neighbors.append(array[curr.x][curr.y+1])
+        return neighbors
+    elif (curr.x == len(array[0])-1 and curr.y == len(array)-1): #Bottom right corner
+        neighbors.append(array[curr.x][curr.y-1])
+        neighbors.append(array[curr.x-1][curr.y])
+        return neighbors
+    elif(curr.x == 0):                                           #Top edge
+        neighbors.append(array[curr.x+1][curr.y])
+        neighbors.append(array[curr.x][curr.y+1])
+        neighbors.append(array[curr.x][curr.y-1])
+        return neighbors
+    elif(curr.x == len(array)-1):                                #Bottom edge
+        neighbors.append(array[curr.x-1][curr.y])
+        neighbors.append(array[curr.x][curr.y+1])
+        neighbors.append(array[curr.x][curr.y-1])
+        return neighbors
+    elif(curr.y == 0):                                            #Left edge
+        neighbors.append(array[curr.x][curr.y+1])
+        neighbors.append(array[curr.x+1][curr.y])
+        neighbors.append(array[curr.x-1][curr.y])
+        return neighbors   
+    elif(curr.y == len(array[0])-1):                             #Right edge
+        neighbors.append(array[curr.x][curr.y-1])
+        neighbors.append(array[curr.x+1][curr.y])
+        neighbors.append(array[curr.x-1][curr.y])
+        return neighbors       
+    else:                                                        #Center node returns 4 neighbors
+        neighbors.append(array[curr.x+1][curr.y])
+        neighbors.append(array[curr.x-1][curr.y])
+        neighbors.append(array[curr.x][curr.y+1])
+        neighbors.append(array[curr.x][curr.y-1])
+        return neighbors
+
 
 
 
