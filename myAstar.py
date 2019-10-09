c3e7d5c6b3327c57aa121c444ccbcf9054227733
Jack <jack@nbp-115-126.nbp.ruw.rutgers.edu>
2019-10-09 19:53:45 -0400

    new code

--- myAstar.py
+++ myAstar.py
@@ -9,19 +9,25 @@ class Node:
     def __init__(self, x, y, blocked):
         self.x = x
         self.y = y
-        self.g = float("inf")
-        self.h = float("inf")
-        self.f = float("inf")
+        self.g = 99999
+        self.h = 99999
+        self.f = 99999
         self.parent = None
         self.blocked = blocked
+        self.visited = False
+        self.onPath =  False
+
 
     def __lt__(self, other): 
         return self.f < other.f
+    def __le__(self, other):
+        return self.f <= other.f
     def __gt__(self, other):
         return self.f > other.f 
+    def __ge__(self, other):
+        return self.f >= other.f
     def __eq__(self, other):
-        return (self.f == other.f) and (self.x == other.x) and (self.y == other.y) and (self.g == other.g) and (self.h == other.h) and (self.parent == other.parent) and (self.blocked == other.blocked)
-        
+        return (self.f == other.f) and (self.x == other.x) and (self.y == other.y) and (self.g == other.g) and (self.h == other.h) and (self.parent == other.parent) and (self.blocked == other.blocked) and (self.visited == other.visited)      
 
 def promptUser():
     #Prompt user for maze number 
@@ -74,110 +80,143 @@ def execute(array, algo):
         openList = []
         closeList = []
 
-        goal = array[4][4]
+        goal = array[len(array)-1][len(array[0])-1]
         curr = array[4][2]
-        curr.g = 0
-        curr.h = manDis(curr.x, curr.y, goal.x, goal.y)
-        curr.f = curr.g + curr.h
+        i = 0
         while(curr != goal):
-
-            path=computePath(curr, goal,array,closeList)
+            i += 1
+            curr = computePath(curr, goal, array, openList, closeList)
+            curr = computePath(curr, goal, array, openList, closeList)
+            
+            return
             #return
-            curr=followPath(path,array,closeList)
-            # curr = computePath(curr, goal, array, openList, closeList) #Returns a list
-            # #Get first blocked cell in path and the parent of this is the new curr 
-            # print("Curr: " + '(' + str(curr.x) + ',' + str(curr.y) + ')')
-            printMaze(curr,array)
+            # closeList.append(curr)
+            # curr = heapq.heappop(openList)
+            #printMaze(curr,array)
         #End while
-
+    #End if
     elif algo == 'b':
         print("Executing Repeated Backward A*")
     else: 
         print("Executing Adaptive A*")
 
-#im at the start of the path and i just keep going and adding to closed list
-def followPath(path, array, closeList):
-    for item in path:
-        if (item.blocked):
-            closeList.append(item)
-            return item.parent
-        closeList.append(item)
-    return item
-
 #Function that takes in "starting" point and computes best path to goal without regard for blocked cells 
-def computePath(start, goal, array, closeList):
-    #so the way this function works is 
-    pol = []
-    pcl = list(closeList)
-    pArray = list(array)
+def computePath(start, goal, array, openList, gCL):
+    print("Start: " + '(' + str(start.x) + ',' + str(start.y) + ')')
+    startx = start.x
+    starty = start.y
+    closeList = list(gCL)
     curr = start
     curr.g = 0
     curr.h = manDis(curr.x, curr.y, goal.x, goal.y)
     curr.f = curr.g + curr.h
-
-    print("Goal: (" +str(goal.x) +","+str(goal.y)+")")
-    while(curr != goal):
-        #print("Current: (" +str(curr.x) +","+str(curr.y)+")")
-        neighbors=getNeighbors(curr, pArray)
-        for item in neighbors:
-            #print("in neighbors for")
-            #print("\tlooking at neighbor: "+ "(" +str(item.x) +","+str(item.y)+")")
-            if (item not in pol and item not in pcl): #item not in pol and
-                #print("\tputting into heap")
-                heapq.heappush(pol, item)
-                heapq.heapify(pol)
-                item.g = item.g + 1
-                item.h = manDis(item.x, item.y, goal.x, goal.y)
-                f = item.g + item.h 
-                item.f = f
-                item.parent = curr
+    #Mark initial neighbors as visited 
+    initialNeighbors = getNeighbors(curr, array)
+    for cell in initialNeighbors:
+        print("Neighbor: " + '(' + str(cell.x) + ',' + str(cell.y) + ') ' + str(cell.blocked))
+        if cell not in openList and cell not in gCL:
+            cell.visited = True
+            if cell.blocked:
+                gCL.append(cell)
+                closeList.append(cell)
             else:
-                pass
-                #print("\t.cell "+ "(" +str(item.x) +","+str(item.y)+") already visited")
-        #End for
-        pcl.append(curr)
-
-        #get next curr
-        curr = heapq.heappop(pol) #can be literally anything THIS SHOULD NEVER HAVE AN "ITS EMPTY" ERROR BC IT SOHULD EVENTUALLY BE FUCKING GOAL AND BREAK OUT 
+                heapq.heappush(openList, cell)
+    gCL.append(curr)
+    i = 0
+    while(curr != goal):
+        i += 1
+        curr.visited = True
+        closeList.append(curr)
+        neighbors = getNeighbors(curr, array)
+        for cell in neighbors:
+            #print("Neighbor: " + '(' + str(cell.x) + ',' + str(cell.y) + ')' + str(cell.visited))
+            if not cell.visited:
+                cell.visited = True
+                cell.g = curr.g + 1
+                cell.h = manDis(cell.x, cell.y, goal.x, goal.y)
+                cell.f = cell.g + cell.h
+                cell.parent = curr
+                heapq.heappush(openList, cell)
+        if(len(openList) != 0):
+            temp = curr
+            curr = heapq.heappop(openList)
+            if(curr == goal):
+                curr.parent = temp
+                break
+            if(i == 1):
+                print("Curr: " + '(' + str(curr.x) + ',' + str(curr.y) + ')')
+                print("closeList: ")
+                printList(closeList)
+                print("openList: ")
+                printList(openList)
+                print("gCL: ")
+                printList(gCL)
+                return
+            #return 
+        else: 
+            print("Path does not exist.")
+            break
+            #return array[goal.x][goal.y]
+        #End if else 
     #End while
-    listPath(goal,pArray)
-    return pathMaker(goal,start,pArray)
-    #call function that makes the path
 
-#End computepath
-    
-def pathMaker(goal, start, array):
-    path=[]
-    ptr=goal
-    while (ptr != start):
-        path.insert(0, ptr)
-        ptr = ptr.parent
-    #adding start to the path
-    path.insert(0,ptr)
-    return path
+    newX, newY = getNewStart(start, goal, array)
 
+    listPath(start,goal,array)
+
+    #Print the path computed from this iteration
+    printMaze(startx, starty,array, gCL)
+
+    #Return new curr (the parent of the last blocked cell going backwards from the goal)
+
+    return array[newX][newY]
+
+#End computePath
+
+
+#Get the new starting point for computePath
+def getNewStart(start, goal, array):
+    result = goal
+    while(goal != None and goal != start):
+        if(goal.blocked):
+            result = goal.parent
+        goal = goal.parent
+    print('New start: (' + str(result.x) + ',' + str(result.y) + ')')
+    return result.x, result.y
 
 
 #List coordinates taken
-def listPath(goal, array):
+def listPath(holder ,goal, array):
     print("Path: \n")
     print("***GOAL***")
-    while(goal.parent != None):
+    while(goal.parent != holder and goal.parent != None):
         print('(' + str(goal.x) + ',' + str(goal.y) + ')')
         goal = goal.parent
+        goal.onPath = True
     print('(' + str(goal.x) + ',' + str(goal.y) + ')')
     print("***START***\n")
 
+
+
+def printList(nodelist):
+    for item in nodelist:
+        print('(' + str(item.x) + ',' + str(item.y) +')' + "f-value: " + str(item.f))
+
+
+
 #Print maze with path
-def printMaze(start,array):
+def printMaze(startx, starty,array,closeList):
     print("Maze:" + '\n' + "----------")
     for line in array:
         for item in line:
-            if(item.x == start.x and item.y == start.y):
+            if(item.x == startx and item.y == starty):
                 sys.stdout.write('S')
             elif(item.x == len(array)-1 and item.y == len(array[0])-1):
                 sys.stdout.write('E')
-            elif(item.blocked):
+            elif(item.onPath):
+                sys.stdout.write('.')
+                item.onPath = False
+            elif(item.blocked and item in closeList):
                 sys.stdout.write('X')
             else:
                 sys.stdout.write(' ')
@@ -242,43 +281,6 @@ promptUser()
 
 
 
-'''
-EXECTUE:
-    
 
 
-    execute runs a loop where we have a closed list and an open list--do we even need a global open list--idts
-    
-    initialize 
-    
-    i never populate closed list in compute path
 
-    after compute path gives me the path i tke that path and send it into the path follower,
-    it takes everything in it and follows the path adding eveything on the way to closed list. it adds the neighbors to to global open list??? idts dont think we need a global open list
-    it adds the last blocked guy to the closed list and returns its parrent--in this fucntion im updating the g h f and partent values of array 
-    in the other function i was just updating the vlaues for the clone, but i need them in the big actual guy--dont even know if inactaully ened this, lets just put it in for now and see if it works
-
-
-    
-
-Computue Path:
-    compute path is going to get the best path to goal taking into account the things in closed list. the things in closed list is not changed in compute path
-
-    first i initialize a start
-    while the start is not the goal:
-        get neighbors:
-            initialize g valus and add to privateopenlist
-        add curr to private closed list
-        pop off best next and set curr to it
-
-    call the fucntion that makes the path get an array of the path from start to goal
-
-    return the path in an array
-
-PathMaker:
-    givin the goal and start and array
-    declare an array for the path
-    loop backward through the paretns getting the coordinates
-    save each item to the front till i reach start.
-
-'''
